91be2eee70b4024fa4f5f93d96ff85dd496a7c04
Juan Martinez <pablomar01@outlook.com>
2023-12-20 21:46:14 -0500

    Patient.vue created

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Header from './components/Header.vue'
   import Form from './components/Form.vue'
+  import Patient from './components/Patient.vue'
   import { reactive, ref } from 'vue';
 
   const patients = ref([])
@@ -31,6 +32,19 @@
         v-model:symptoms="pet.symptoms"
         @save-patient="savePatient"
       />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Patient list</h3>
+
+        <div v-if="patients.length > 0">
+          <p class="text-lg text-center mt-5 mb-10">
+            <span class="text-indigo-600 font-bold">Patient </span>
+            Information
+          </p>  
+          <Patient v-for="patient in patients" :patient="patient" />
+        </div>
+        <p v-else class="mt-20 text-2xl text-center">There are no patients yet</p>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/Patient.vue
@@ -0,0 +1,64 @@
+<script setup>
+  defineProps({
+    patient: {
+      type: Object,
+      required: true,
+    }
+  })
+</script>
+
+<template>
+  <div class="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p class="font-bold mb-3 text-gray-700 uppercase">ID:
+          <span class="font-normal normal-case">
+          
+          </span>
+      </p>
+
+      <p class="font-bold mb-3 text-gray-700 uppercase">Name:
+          <span class="font-normal normal-case">
+            {{ patient.name }}
+          </span>
+      </p>
+
+      <p class="font-bold mb-3 text-gray-700 uppercase">Owner: 
+          <span class="font-normal normal-case">
+            {{ patient.owner }}
+          </span>
+      </p>
+
+      <p class="font-bold mb-3 text-gray-700 uppercase">Email: 
+          <span class="font-normal normal-case">
+            {{ patient.email }}
+          </span>
+      </p>
+
+      <p class="font-bold mb-3 text-gray-700 uppercase">Arrival date:
+          <span class="font-normal normal-case">
+            {{ patient.arrival }}
+          </span>
+      </p>
+
+      <p class="font-bold mb-3 text-gray-700 uppercase">Symptoms:
+          <span class="font-normal normal-case">
+            {{ patient.symptoms }}
+          </span>
+      </p>
+
+      <div class="grid md:grid-cols-2  gap-5 mt-10 ">
+          <button 
+              type="button"
+              class="block w-full py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          >
+            Edit
+          </button>
+
+          <button 
+              type="button"
+              class="block w-full py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          >
+            Delete
+          </button>
+      </div>
+  </div>
+</template>
